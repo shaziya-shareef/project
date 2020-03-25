574d6e61a9f2f72e3c3561b884036a2af5f99303
shaziya <62055256+shaziya-shareef@users.noreply.github.com>
2020-03-25 13:32:46 +0530

    Add files via upload

---
+++ product-info.ts
@@ -0,0 +1,12 @@
+export class ProductInfo {
+	product_id:string;
+	category_type: number;
+	create_time:Date;
+	product_description:string;
+	product_icon:string;
+	product_name:string;
+	product_price:Float32Array;
+	product_status:number;
+	product_stock:number;
+	update_time:Date;
+}
